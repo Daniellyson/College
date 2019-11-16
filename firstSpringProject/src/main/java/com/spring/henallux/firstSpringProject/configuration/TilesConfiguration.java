bd85fe57d952ac704ebb2932dcb6f5874aeb962e
Daniellyson <etu32290@henallux.be>
2019-11-16 11:33:06 +0100

    login webSecurity

--- firstSpringProject/src/main/java/com/spring/henallux/firstSpringProject/configuration/TilesConfiguration.java
+++ firstSpringProject/src/main/java/com/spring/henallux/firstSpringProject/configuration/TilesConfiguration.java
@@ -3,6 +3,7 @@ package com.spring.henallux.firstSpringProject.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
 import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
 import org.springframework.web.servlet.view.tiles3.TilesView;
@@ -12,7 +13,7 @@ public class TilesConfiguration {
     @Bean
     public TilesConfigurer tilesConfigurer() {
         final TilesConfigurer configurer = new TilesConfigurer();
-        configurer.setDefinitions("WEB-INF/resources/tiles.xml");
+        configurer.setDefinitions("/WEB-INF/resources/tiles.xml");
         configurer.setCheckRefresh(true);
         return configurer;
     }
--- firstSpringProject/src/main/java/com/spring/henallux/firstSpringProject/controller/WelcomeController.java
+++ firstSpringProject/src/main/java/com/spring/henallux/firstSpringProject/controller/WelcomeController.java
@@ -1,60 +1,16 @@
 package com.spring.henallux.firstSpringProject.controller;
 
-import com.spring.henallux.firstSpringProject.dataAccess.MagicKeyDataAccess;
-import com.spring.henallux.firstSpringProject.dataAccess.dao.MagicKeyDAO;
-import com.spring.henallux.firstSpringProject.model.MagicKeyForm;
-import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-
 @Controller
 @RequestMapping(value="/hello")
-@AllArgsConstructor
 public class WelcomeController {
-
-    @Autowired
-    private MessageSource messageSource;
-    @Autowired
-    private MagicKeyDataAccess magicKeyDataAccess;
-
-    @RequestMapping(method = RequestMethod.GET)
-    public String home(Model model, Locale locale) {
-        //model.addAttribute();
-        model.addAttribute("title", "Dany Spring boot");
-        model.addAttribute("locale", locale.getLanguage());
-        model.addAttribute("formMagicKey", new MagicKeyForm());
+    @RequestMapping (method = RequestMethod.GET)
+    public String home (Model model) {
+        //return "welcome";
         return "integrated:welcome";
     }
-
-    @RequestMapping(value="/sent", method = RequestMethod.POST)
-    public String getFormData(@ModelAttribute(value = "formMagicKey") MagicKeyForm magicKeyForm, Locale locale) {
-
-        String message = messageSource.getMessage("badKey", null, locale);
-
-        ArrayList<MagicKeyForm> keysDB = magicKeyDataAccess.getMagicKey();
-
-        int countKey = 0;
-        while(countKey < keysDB.size() &&
-                !keysDB.get(countKey).getMagicKey().equals(magicKeyForm.getMagicKey())) {
-            countKey++;
-        }
-
-        if (countKey < keysDB.size() && keysDB.get(countKey).getMagicKey().equals(magicKeyForm.getMagicKey())) {
-            return "redirect:/userInscription";
-        }
-        else {
-
-            return "integrated:keyError";
-        }
-    }
-
 }
